1e2668e2b52a7c6470588f903aab6fd2db0ae836
moudisoubra <moudisoubra001@gmail.com>
2019-10-05 23:39:08 +0400

    no message

--- Assets/Scripts/ToggleSwitch.cs
+++ Assets/Scripts/ToggleSwitch.cs
@@ -55,6 +55,7 @@ public class ToggleSwitch : MonoBehaviour
     public void RegisterUser()
     {
         //AddUser/:userID/:userName/:userGender/:userSeniority/:userHouse/:userLogin/:userPassword
+        //https://testserversoubra.herokuapp.com/AddUser/2/ExampleName/Male/CEO/Green/ExampleLogin/E123
         Debug.Log("https://testserversoubra.herokuapp.com/AddUser/"
                 + userID + "/" + userName + "/" + userGender + "/" + userSeniority + "/" + userHouse + "/" + userLogin + "/" + userPassword);
 

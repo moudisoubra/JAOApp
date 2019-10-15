a273de42f4986a49831507608251e0c4c16d962a
moudisoubra <moudisoubra001@gmail.com>
2019-10-15 17:47:42 +0400

    Added Transitions

--- Assets/Scripts/ToggleButtons.cs
+++ Assets/Scripts/ToggleButtons.cs
@@ -14,12 +14,19 @@ public class ToggleButtons : MonoBehaviour
     public GameObject IdeaGeneratorButton;
     public GameObject CommunicationButton;
     public GameObject ToolsButton;
+    public GameObject HomeButton;
+    public GameObject ProfileButton;
 
     public GameObject ReadingRoomButtonPlaceHolder;
     public GameObject StatisticsButtonPlaceHolder;
     public GameObject IdeaGeneratorButtonPlaceHolder;
     public GameObject CommunicationButtonPlaceHolder;
     public GameObject ToolsButtonPlaceHolder;
+    public GameObject HomeButtonPlaceHolder;
+    public GameObject ProfileButtonPlaceHolder;
+
+    public GameObject shade;
+    public GameObject[] allPanels;
 
     public float lerpSpeed;
     public bool spread;
@@ -36,12 +43,31 @@ public class ToggleButtons : MonoBehaviour
         if (spread)
         {
             SpreadButtons();
+            shade.SetActive(true);
             AppButton.GetComponent<Image>().sprite = XButton;
+            EnlargeButtons();
         }
         else if (!spread)
         {
             ReturnButtons();
+            shade.SetActive(false);
             AppButton.GetComponent<Image>().sprite = appButton;
+            SmallButtons();
+        }
+    }
+
+    public void ChangeToPanel(string panelName)
+    {
+        for (int i = 0; i < allPanels.Length; i++)
+        {
+            if (allPanels[i].name == panelName)
+            {
+                allPanels[i].SetActive(true);
+            }
+            else
+            {
+                allPanels[i].SetActive(false);
+            }
         }
     }
 
@@ -56,6 +82,8 @@ public class ToggleButtons : MonoBehaviour
         IdeaGeneratorButton.transform.position = Vector3.Lerp(IdeaGeneratorButton.transform.position, IdeaGeneratorButtonPlaceHolder.transform.position, lerpSpeed);
         CommunicationButton.transform.position = Vector3.Lerp(CommunicationButton.transform.position, CommunicationButtonPlaceHolder.transform.position, lerpSpeed);
         ToolsButton.transform.position = Vector3.Lerp(ToolsButton.transform.position, ToolsButtonPlaceHolder.transform.position, lerpSpeed);
+        HomeButton.transform.position = Vector3.Lerp(HomeButton.transform.position, HomeButtonPlaceHolder.transform.position, lerpSpeed);
+        ProfileButton.transform.position = Vector3.Lerp(ProfileButton.transform.position, ProfileButtonPlaceHolder.transform.position, lerpSpeed);
     }
 
     public void ReturnButtons()
@@ -65,5 +93,23 @@ public class ToggleButtons : MonoBehaviour
         IdeaGeneratorButton.transform.position = Vector3.Lerp(IdeaGeneratorButton.transform.position, AppButton.transform.position, lerpSpeed);
         CommunicationButton.transform.position = Vector3.Lerp(CommunicationButton.transform.position, AppButton.transform.position, lerpSpeed);
         ToolsButton.transform.position = Vector3.Lerp(ToolsButton.transform.position, AppButton.transform.position, lerpSpeed);
+        HomeButton.transform.position = Vector3.Lerp(HomeButton.transform.position, AppButton.transform.position, lerpSpeed);
+        ProfileButton.transform.position = Vector3.Lerp(ProfileButton.transform.position, AppButton.transform.position, lerpSpeed);
+    }
+
+    public void EnlargeButtons()
+    {
+
+            HomeButton.transform.localScale = Vector3.Lerp(HomeButton.transform.localScale, HomeButtonPlaceHolder.transform.localScale, lerpSpeed);
+            ProfileButton.transform.localScale = Vector3.Lerp(ProfileButton.transform.localScale, ProfileButtonPlaceHolder.transform.localScale, lerpSpeed);
+        
+    }
+
+    public void SmallButtons()
+    {
+
+            HomeButton.transform.localScale = Vector3.Lerp(HomeButton.transform.localScale, AppButton.transform.localScale, lerpSpeed);
+            ProfileButton.transform.localScale = Vector3.Lerp(ProfileButton.transform.localScale, AppButton.transform.localScale, lerpSpeed);
+        
     }
 }

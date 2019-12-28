1616369fb0374eda9fc459ccc5174e0714b5515d
Mohammad Soubra <moudisoubra001@gmail.com>
2019-12-28 13:07:49 +0400

    no message

---
+++ Assets/Algorithm/AssignTeam.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssignTeam : MonoBehaviour
+{
+    public 
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void AddMember( )
+    {
+
+    }
+}
---
+++ Assets/Algorithm/InheritedTeam.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InheritedTeam : Team
+{
+    public TextMeshProUGUI totalCount;
+    public TextMeshProUGUI maleCount;
+    public TextMeshProUGUI femaleCount;
+    public TextMeshProUGUI oA;
+    public TextMeshProUGUI oB;
+    public TextMeshProUGUI oC;
+    public TextMeshProUGUI oD;
+
+    void Update()
+    {
+        totalCount.text = totalMembers.ToString();
+        maleCount.text = numberOfMales.ToString();
+        femaleCount.text = numberOfFemales.ToString();
+        oA.text = numberOccupationA.ToString();
+        oB.text = numberOccupationB.ToString();
+        oC.text = numberOccupationC.ToString();
+        oD.text = numberOccupationD.ToString();
+    }
+}

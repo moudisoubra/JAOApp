f36cf05585b7da7faaf335a7c093f361b43dab82
moudisoubra <moudisoubra001@gmail.com>
2019-12-30 10:45:05 +0400

    Started Block Falling

---
+++ Assets/Scripts/TetrisGame/MoveBlock.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBlock : MonoBehaviour
+{
+    public float previousTime;
+    public float fallTime = 0.8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MoveLeft();
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveRight();
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            fallTime = fallTime / 10;
+        }
+        else
+        {
+            fallTime = 0.8f;
+        }
+
+        if (Time.time - previousTime > fallTime)
+        {
+            FallingTetris();
+        }
+
+    }
+    public void MoveRight()
+    {
+        transform.position += new Vector3(100, 0, 0);
+    }
+
+    public void MoveLeft()
+    {
+        transform.position += new Vector3(-100, 0, 0);
+    }
+
+    public void FallingTetris()
+    {
+        transform.position += new Vector3(0, -100, 0);
+        previousTime = Time.time;
+    }
+}

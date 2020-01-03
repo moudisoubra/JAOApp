4149be0f8c3aadb401caa8b3fce7103a8f44ba83
moudisoubra <moudisoubra001@gmail.com>
2020-01-03 15:15:30 +0400

    Tappy Plane Finished

---
+++ Assets/Scripts/FlappyPlane/Flap.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Flap : MonoBehaviour
+{
+    public Swipe swipe;
+    public Rigidbody2D rb;
+    public float velocity = 1;
+    public int points;
+    public bool startTapping;
+    public Text scoreText;
+    public GameObject endButton;
+
+    void Start()
+    {
+        startTapping = true;
+        rb = GetComponent<Rigidbody2D>();
+        swipe = FindObjectOfType<Swipe>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!startTapping)
+        {
+            endButton.SetActive(true);
+        }
+        if (swipe.Tap && startTapping)
+        {
+            rb.velocity = Vector2.up * velocity;
+        }
+        scoreText.text = points.ToString("0");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Point"))
+        {
+            points += 1;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            startTapping = false;
+        }
+    }
+}
---
+++ Assets/Scripts/FlappyPlane/TappyObstacleSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TappyObstacleSpawner : MonoBehaviour
+{
+    public float maxTime = 1;
+    public float timer = 0;
+    public float height;
+    public GameObject obstaclePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newObstacle = Instantiate(obstaclePrefab);
+        newObstacle.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+        Destroy(newObstacle, 25);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > maxTime)
+        {
+            GameObject newObstacle = Instantiate(obstaclePrefab);
+            newObstacle.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+            Destroy(newObstacle, 25);
+            timer = 0;
+        }
+
+        timer += Time.deltaTime;
+    }
+}
